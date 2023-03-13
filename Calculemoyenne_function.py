38d87b61fc89e4ddc49ac0f5e4ba9667c9edad45
dannkamenan1995 <dannkamenan@hotmail.com>
2023-03-13 12:57:12 +0000

    Mise à jour

--- Calculemoyenne_function.py
+++ Calculemoyenne_function.py
@@ -1,8 +1,19 @@
 import tkinter as tk
 
 def calculeMoyenne(Algo, Python, Php, Javascript):
+    """_summary_
+
+    Args:
+        Algo (_int_): _Note de l'étudiant en Algoritme_
+        Python (_int_): _Note de l'étudiant en Python_
+        Php (_int_): _Note de l'étudiant en PHP_
+        Javascript (_int_): _Note de l'étudiant en Java_
+
+    Returns:
+        _type_: _description_
+    """
     Moyenne = (Python + Algo + Php + Javascript) / 4
-    return Moyenne
+    return Moyenne  
 
 def Boutton_click():
     Py = float(txtpy.get("1.0", "end-1c"))
@@ -21,8 +32,8 @@ def Boutton_click():
     elif moy>14 and moy<=16 :
         mention_label.config(text="Mention= Bien")
     elif moy>16:
-        mention_label.config(text="Mention= Très-bien")
-
+        mention_label.config(text="Mention= Très-bien")  
+         
 Menu1 = tk.Tk()
 Menu1.maxsize
 Titre = tk.Label(Menu1, text="LOGICIEL DE CALCULE DE MOYENNE")
